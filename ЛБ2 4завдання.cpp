ff8d35550103b782c70c2962b9661d09dd4bb0fa
Kiril Rogalsky <kirilllrogalsky@gmail.com>
2024-10-05 11:24:27 +0300

    Add files via upload

---
+++ ЛБ2 4завдання.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(0, ".1251");
+    int N;
+
+    
+    cout << "Введiть число N: ";
+    cin >> N;
+
+    cout << "Простi числа вiд 1 до " << N << ": ";
+
+   
+    for (int num = 2; num <= N; ++num) {
+        bool isPrime = true;
+
+        
+        for (int i = 2; i * i <= num; ++i) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        
+        if (isPrime) {
+            cout << num << " ";
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
