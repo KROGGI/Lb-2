184c9d143ddb9c16c6b0f15ffa547e0e7c29287c
Kiril Rogalsky <kirilllrogalsky@gmail.com>
2024-10-05 11:18:20 +0300

    Add files via upload

---
+++ ЛБ2 3завдання.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(0, ".1251");
+    int height;
+
+    
+    cout << "Введiть висоту трикутника: ";
+    cin >> height;
+
+    
+    for (int i = 1; i <= height; ++i) { 
+        for (int j = 1; j <= i; ++j) {  
+            cout << "*";
+        }
+        cout << endl; 
+    }
+
+    return 0;
+}
