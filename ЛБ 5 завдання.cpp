899b9c0d789caf86ef9354b5a7e026a9af03f6d7
Kiril Rogalsky <kirilllrogalsky@gmail.com>
2024-10-05 11:27:35 +0300

    Add files via upload

---
+++ ЛБ 5 завдання.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(0, ".1251");
+    int grade;
+
+    // Запитуємо оцінку студента
+    cout << "Введiть оцiнку студента (від 0 до 100): ";
+    cin >> grade;
+
+    // Перевіряємо діапазон оцінок і виводимо відповідну характеристику
+    if (grade >= 90 && grade <= 100) {
+        cout << "Вiдмiнно" << endl;
+    }
+    else if (grade >= 75 && grade < 90) {
+        cout << "Добре" << endl;
+    }
+    else if (grade >= 60 && grade < 75) {
+        cout << "Задовiльно" << endl;
+    }
+    else if (grade >= 0 && grade < 60) {
+        cout << "Незадовiльно" << endl;
+    }
+    else {
+        // Якщо введене значення не входить в діапазон від 0 до 100
+        cout << "Некоректна оцiнка. Введiть значення вiд 0 до 100." << endl;
+    }
+
+    return 0;
+}
